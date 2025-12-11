38255e0d5618efb4eeaabe04cb56c3dd47b5349d
anish <pogchampoo24@gmail.com>
2025-12-11 07:40:23 +0530

    open and listen on local socket for file name to transcribe

--- whisper_daemon.py
+++ whisper_daemon.py
@@ -1,9 +1,33 @@
 import whisper
+import socket
+import os
+import atexit
+
+SOCKET_FILE = "/tmp/whisper_daemon.sock"
 
 def main():
+    delete_socket_file()
+
+    server = socket.socket(socket.AF_UNIX)
+    server.bind(SOCKET_FILE)
+    print("Socket initialized")
+
     model = whisper.load_model("small")
-    result = model.transcribe("test2.wav")
-    print(result["text"])
+    print("Model loaded")
+
+    while True:
+        server.listen()
+        conn, _ = server.accept()
+
+        audio_file_to_transcribe = conn.recv(1024).decode('utf-8').strip()
+        result = model.transcribe(audio_file_to_transcribe)
+        print(result["text"])
+
+def delete_socket_file():
+    if os.path.exists(SOCKET_FILE):
+        os.remove(SOCKET_FILE)
 
 if __name__ == "__main__":
+    atexit.register(delete_socket_file)
     main()
+
