3b64b00d706dad69de1b272fc88636193ffa65f5
anish <pogchampoo24@gmail.com>
2025-12-11 12:15:34 +0530

    consolidate recording and transcript into one lua file

---
+++ nvim_client/plugin.lua
@@ -0,0 +1,56 @@
+local ffi = require("ffi")
+
+function get_transcription()
+	ffi.cdef([[
+    char* transcribe_audio(const char* audio_file_path, const char* socket_file_path);
+]])
+	local lib = ffi.load("/home/anishs/development/voice_to_code/rust_client/target/release/libtranscript_processor.so")
+	local transcript = lib.transcribe_audio("/tmp/nvim_recording.wav", "/tmp/whisper_daemon.sock")
+	return ffi.string(transcript)
+end
+
+local record_cmd = {
+	"arecord",
+	"-f",
+	"S16_LE",
+	"-c",
+	1,
+	"-r",
+	16000,
+	"/tmp/nvim_recording.wav",
+}
+
+local recording_job = nil
+
+local function start_recording()
+	recording_job = vim.fn.jobstart(record_cmd)
+end
+
+local function stop_recording()
+	vim.fn.jobstop(recording_job)
+	recording_job = nil
+end
+
+local function toggle_recording_and_append()
+	if recording_job == nil then
+		start_recording()
+		print("recording started")
+	else
+		stop_recording()
+		print("recording stopped")
+
+		print("generating transcript")
+		local transcript = get_transcription()
+		print("appending transcript")
+		print(transcript)
+		vim.schedule(function()
+			local line_count = vim.api.nvim_buf_line_count(0)
+			local lines = vim.split(transcript, "\n", { plain = true })
+			vim.api.nvim_buf_set_lines(0, line_count, line_count, false, lines)
+			vim.notify("✅ Transcription appended!", vim.log.levels.INFO)
+		end)
+	end
+end
+
+vim.api.nvim_create_user_command("VoiceToggle", toggle_recording_and_append, {})
+vim.keymap.set("n", "<leader>vt", toggle_recording_and_append, { desc = "Voice: Toggle record" })
