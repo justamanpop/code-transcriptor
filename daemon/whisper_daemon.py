17309ad921c12af60f10a444e8ca676d12faf596
anish <pogchampoo24@gmail.com>
2025-12-11 07:57:26 +0530

    made model size CLI arg

--- daemon/whisper_daemon.py
+++ daemon/whisper_daemon.py
@@ -1,23 +1,34 @@
-import whisper
+import argparse
 import socket
 import os
 import atexit
 
-SOCKET_FILE = "/tmp/whisper_daemon.sock"
+import whisper
+
+SOCKET_FILE_PATH = "/tmp/whisper_daemon.sock"
 
 def delete_socket_file():
-    if os.path.exists(SOCKET_FILE):
-        os.remove(SOCKET_FILE)
+    if os.path.exists(SOCKET_FILE_PATH):
+        os.remove(SOCKET_FILE_PATH)
+
+def parse_cli_args():
+    parser = argparse.ArgumentParser(description="Daemon that loads the whisper model and listens for audio file paths to transcribe")
+    parser.add_argument("--model-size", type=str, default="small", help="size of whisper_model to use", choices = {"small", "base"})
+    args = parser.parse_args()
+    return (args.model_size)
 
 def main():
+    (model_size) = parse_cli_args()
+
     delete_socket_file()
 
     server = socket.socket(socket.AF_UNIX)
-    server.bind(SOCKET_FILE)
+    server.bind(SOCKET_FILE_PATH)
     print("Socket initialized")
 
-    model = whisper.load_model("small")
-    print("Model loaded")
+    final_model_size = f'{model_size}.en'
+    model = whisper.load_model(final_model_size)
+    print(f"Model {final_model_size} loaded")
 
     while True:
         server.listen()
