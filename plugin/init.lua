f9dc7bd4724fff848ba0991b89c7e8f6f92cd565
anish <pogchampoo24@gmail.com>
2025-12-12 09:46:42 +0530

    plugin updates

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,10 +1,12 @@
 local ffi = require("ffi")
+local async = require("plenary.async")
 
-function get_transcription()
-	ffi.cdef([[
+ffi.cdef([[
     char* transcribe_audio(const char* audio_file_path, const char* socket_file_path);
 ]])
-	local lib = ffi.load("/home/anishs/development/voice_to_code/rust_client/target/release/libtranscript_processor.so")
+local lib = ffi.load("/home/anishs/development/voice_to_code/rust_client/target/release/libtranscript_processor.so")
+
+function get_transcription()
 	local transcript = lib.transcribe_audio("/tmp/nvim_recording.wav", "/tmp/whisper_daemon.sock")
 	return ffi.string(transcript)
 end
@@ -31,22 +33,22 @@ local function stop_recording()
 	recording_job = nil
 end
 
+local function on_stop_recording()
+	print("recording stopped, generating transcript")
+	local transcript = get_transcription()
+	print("generated transcript, writing to file")
+	local line_count = vim.api.nvim_buf_line_count(0)
+	local lines = vim.split(transcript, "\n", { plain = true })
+	vim.api.nvim_buf_set_lines(0, line_count, line_count, false, lines)
+end
+
 local function toggle_recording_and_append()
 	if recording_job == nil then
 		start_recording()
 		print("recording started")
 	else
 		stop_recording()
-		print("recording stopped, generating transcript")
-		vim.loop
-			.new_work(get_transcription, function(transcript)
-				local line_count = vim.api.nvim_buf_line_count(0)
-				-- Split text by newlines for proper multi-line appending
-				local lines = vim.split(transcript, "\n", { plain = true })
-				vim.api.nvim_buf_set_lines(0, line_count, line_count, false, lines)
-			end)
-			:queue()
-		vim.loop.run()
+		async.run(on_stop_recording)
 	end
 end
 
