1f5454976c203dc305ce3cca668fdaad2fcc8ab5
anish <pogchampoo24@gmail.com>
2025-12-12 09:52:10 +0530

    update

--- plugin/init.lua
+++ plugin/init.lua
@@ -35,11 +35,7 @@ end
 
 local function on_stop_recording()
 	print("recording stopped, generating transcript")
-	local transcript = get_transcription()
-	print("generated transcript, writing to file")
-	local line_count = vim.api.nvim_buf_line_count(0)
-	local lines = vim.split(transcript, "\n", { plain = true })
-	vim.api.nvim_buf_set_lines(0, line_count, line_count, false, lines)
+	return get_transcription()
 end
 
 local function toggle_recording_and_append()
@@ -48,7 +44,13 @@ local function toggle_recording_and_append()
 		print("recording started")
 	else
 		stop_recording()
-		async.run(on_stop_recording)
+		async.run(on_stop_recording, function(transcript)
+			print("generated transcript, writing to file")
+			local line_count = vim.api.nvim_buf_line_count(0)
+			local lines = vim.split(transcript, "\n", { plain = true })
+			vim.api.nvim_buf_set_lines(0, line_count, line_count, false, lines)
+		end)
+		vim.notify("NO WAIT", vim.log.levels.INFO)
 	end
 end
 
