4a1f22a385bf1af5a2c7d214d581f0c63d3492a2
anish <pogchampoo24@gmail.com>
2025-12-12 10:14:12 +0530

    plugin updates

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,16 +1,14 @@
-local ffi = require("ffi")
-local async = require("plenary.async")
-
-ffi.cdef([[
-    char* transcribe_audio(const char* audio_file_path, const char* socket_file_path);
-]])
-local lib = ffi.load("/home/anishs/development/voice_to_code/rust_client/target/release/libtranscript_processor.so")
+local uv = vim.uv or vim.loop
 
 function get_transcription()
+	local ffi = require("ffi")
+	ffi.cdef([[
+    char* transcribe_audio(const char* audio_file_path, const char* socket_file_path);
+]])
+	local lib = ffi.load("/home/anishs/development/voice_to_code/rust_client/target/release/libtranscript_processor.so")
 	local transcript = lib.transcribe_audio("/tmp/nvim_recording.wav", "/tmp/whisper_daemon.sock")
 	return ffi.string(transcript)
 end
-
 local record_cmd = {
 	"arecord",
 	"-f",
@@ -39,12 +37,12 @@ local function toggle_recording_and_append()
 		print("recording started")
 	else
 		stop_recording()
-		async.util.scheduler.run_in_thread(get_transcription, function(transcript)
+		uv.new_work(get_transcription, function()
 			print("generated transcript, writing to file")
 			local line_count = vim.api.nvim_buf_line_count(0)
 			local lines = vim.split(transcript, "\n", { plain = true })
 			vim.api.nvim_buf_set_lines(0, line_count, line_count, false, lines)
-		end)
+		end)()
 	end
 end
 
