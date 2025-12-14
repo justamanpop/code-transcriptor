ffd5846f90d8a77cb683ea67ccf29950de7682d9
anish <pogchampoo24@gmail.com>
2025-12-14 13:34:35 +0530

    passing vim.bo.filetype to rust client

--- plugin/init.lua
+++ plugin/init.lua
@@ -7,7 +7,7 @@ function get_transcription()
     void free_string(char* s);
 ]])
 	local lib = ffi.load("/home/anishs/development/voice_to_code/rust_client/target/release/libtranscript_processor.so")
-	local response = lib.transcribe_audio("/tmp/nvim_recording.wav", "/tmp/whisper_daemon.sock", "lua")
+	local response = lib.transcribe_audio("/tmp/nvim_recording.wav", "/tmp/whisper_daemon.sock", vim.bo.filetype)
 	local transcript = ffi.string(response)
 	lib.free_string(response)
 	return transcript
