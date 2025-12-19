5e46d033dc7a6e6ea4c521b8cd1c2d5ad09f0d96
anish <pogchampoo24@gmail.com>
2025-12-19 11:50:01 +0530

    handle "space" case for strings

--- rust_client/src/go/tests.rs
+++ rust_client/src/go/tests.rs
@@ -3,7 +3,7 @@ mod tests {
     use super::super::clean_transcript;
 
     #[test]
-    fn test_clean_transcript_single_line_one_string_assignment() -> Result<(), String> {
+    fn test_clean_transcript_single_line_string_assignment() -> Result<(), String> {
         //arrange
         let input = String::from("a colon equals \"hello\"");
 
@@ -17,7 +17,7 @@ mod tests {
     }
 
     //This is tested because transcript returned by whisper does have spaces between the word
-    //double quotes and the actual string itself
+    //double quotes and the actual string itself. Clean should remove that
     #[test]
     fn test_clean_transcript_single_line_one_string_with_leading_and_trailing_space_assignment() -> Result<(), String> {
         //arrange
@@ -28,6 +28,21 @@ mod tests {
 
         //assert
         let expected = "a := \"hello\"\n";
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    //If "space" is found, it should be replaced with " "
+    #[test]
+    fn test_clean_transcript_handles_word_space_inside_double_quotes() -> Result<(), String> {
+        //arrange
+        let input = String::from("a colon equals \" space \"");
+
+        //act
+        let actual = clean_transcript(input);
+
+        //assert
+        let expected = "a := \" \"\n";
         assert_eq!(expected, actual);
         Ok(())
     }
